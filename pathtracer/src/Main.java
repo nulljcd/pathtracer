8b10684d7844881e9eedc6c0b38914e59329e947
nulljcd <nulljcd@gmail.com>
2024-07-04 13:11:49 -0700

    Update Main.java

--- pathtracer/src/Main.java
+++ pathtracer/src/Main.java
@@ -1,13 +1,15 @@
 class Main {
   public static void main(String[] args) {
+    // final int viewportWidth = 1920;
+    // final int viewportHeight = 1080;
     final int viewportWidth = 1024;
     final int viewportHeight = 640;
 
     Viewport viewport = new Viewport(viewportWidth, viewportHeight);
 
     Scene scene = new Scene();
-
-    scene.add(new AmbientLight(new Vector3(.2, .3, .4), 1));
+    
+    scene.add(new AmbientLight(new Vector3(.3, .4, .5), .4));
 
     scene.add(new Camera(
 			new Vector3(0, 0, 0),
@@ -15,36 +17,6 @@ class Main {
 			viewportWidth, viewportHeight));
 
     Mesh mesh0 = new Mesh(
-      new Vector3(0, 0, -6), // position
-      new Vector3(1, 1, 1), // scale
-      new Vector3(0, -.5, 0), // rotation
-      OBJLoader.load("res/monkey.obj"), // triangles
-      new Material(
-        new Vector3(0, 0, 0), // color
-        new Vector3(.8, .8, .8), // clear coat color
-        1, // clear coat weight
-        .4, // clear coat roughness
-        new Vector3(0, 0, 0), // emission color
-        0)); // emission strength
-
-    scene.add(mesh0);
-
-    Mesh mesh1 = new Mesh(
-      new Vector3(-10, 6, -6),
-      new Vector3(3, 3, 3),
-      new Vector3(0, 0, 0),
-      OBJLoader.load("res/sphere.obj"),
-      new Material(
-        new Vector3(0, 0, 0),
-        new Vector3(0, 0, 0),
-        0, 
-        0,
-        new Vector3(1, .6, .1),
-        4));
-
-    scene.add(mesh1);
-
-    Mesh mesh2 = new Mesh(
       new Vector3(0, -1, -6),
       new Vector3(10, 1, 10),
       new Vector3(0, 0, 0),
@@ -57,16 +29,38 @@ class Main {
         new Vector3(0, 0, 0),
         0));
 
+    scene.add(mesh0);
+
+    Mesh mesh1 = new Mesh(
+      new Vector3(0, 0, -6),
+      new Vector3(1, 1, 1),
+      new Vector3(0, 0, 0),
+      OBJLoader.load("res/sphere.obj"),
+      new Material(
+        new Vector3(1, .5, .7),
+        new Vector3(0, 0, 0),
+        0, 
+        0,
+        new Vector3(0, 0, 0),
+        0));
+
+    scene.add(mesh1);
+
+    Mesh mesh2 = new Mesh(
+      new Vector3(-10, 6, -6),
+      new Vector3(3, 3, 3),
+      new Vector3(0, 0, 0),
+      OBJLoader.load("res/sphere.obj"),
+      new Material(
+        new Vector3(0, 0, 0),
+        new Vector3(0, 0, 0),
+        0, 
+        0,
+        new Vector3(1, .6, .2),
+        8));
+
     scene.add(mesh2);
 
-    // --- options for rendering ---
-    // viewport,
-    // scene,
-    // rays per pixel,
-    // max bounces for a ray
-    // tone mapping type
-    // render block size
-    // number of threads
-    Renderer.render(viewport, scene, 50, 6,1, 64, 4);
+    Renderer.render(viewport, scene, 1000, 10,1, 64, 4);
   }
 }
